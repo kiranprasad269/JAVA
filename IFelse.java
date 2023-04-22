96c28d1bbd4036a81275ee60febec7d57e09ea74
Kiranprasad R <63505355+kiranprasad269@users.noreply.github.com>
2023-04-22 18:02:02 +0530

    Add files via upload

---
+++ IFelse.java
@@ -0,0 +1,13 @@
+import javax.swing.plaf.metal.MetalLookAndFeel;
+
+public class IFelse {
+    public static void main(String[] args){
+        int a=5;
+        if(a==10) {
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
